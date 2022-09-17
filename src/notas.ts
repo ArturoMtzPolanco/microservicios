6e044be6b1001476412e52542eb181033902e413
Arq. Software <20393160@utcancun.edu.mx>
2022-09-17 01:12:40 -0500

    notas

---
+++ src/notas.ts
@@ -0,0 +1,5 @@
+// se utilizo el comando "ionic start micriservicios" para la creacion del proyecto
+
+// se tulizo el comando "ionic serve" para levantar el servicio
+
+// se ultilo la documentacion de ionic para poner un boton 
